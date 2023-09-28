a58f8966dc08603087cbda9726d2e5d6101eac3a
Akhaladze <4468618@gmail.com>
2023-09-28 16:17:08 +0300

    -- add menu

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+@app.route('/menu')
+def getData():
+
+    return '{"{Command1":"Tetst"}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0:5030')
